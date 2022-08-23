d5641e2ad219cfd6c78551ad140c6b14b3ef24aa
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 23:54:28 +0530

    ADDED: Number Of Times have to Flip The Coin

--- src/com/basiccoreprograms/FlipCoin.java
+++ src/com/basiccoreprograms/FlipCoin.java
@@ -1,10 +1,26 @@
 package com.basiccoreprograms;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class FlipCoin {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Flip Coin Problem");
-
+		
+		// INPUT number of times to flip the coin user should have to enter positive integer greater than ZERO
+		Scanner scanner = new Scanner(System.in);
+		int numberOfTimes = 0;
+		while (numberOfTimes <= 0) {
+			System.out.print("Enter number fo times to flip the coin : ");
+			numberOfTimes = scanner.nextInt();
+			if (numberOfTimes < 0) {
+				System.out.println("Please enter positive number");
+			}
+		}
+		
+		System.out.println(numberOfTimes+" times have to flip the coin");
+		
 	}
 
 }
