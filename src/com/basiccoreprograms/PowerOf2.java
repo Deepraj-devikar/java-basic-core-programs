fc3f6fdfb0365b6f1b03f92996ad036cc3a0abde
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 09:56:24 +0530

    ADDED: INPUT positive integer to show upto Power Of 2

--- src/com/basiccoreprograms/PowerOf2.java
+++ src/com/basiccoreprograms/PowerOf2.java
@@ -1,10 +1,22 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class PowerOf2 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Power Of 2 Program");
-
+		
+		// INPUT Nth number up to which calculate Power Of 2 Table
+		Scanner scanner = new Scanner(System.in);
+		int nth = 0;
+		System.out.print("Enter positive number : ");
+		nth = scanner.nextInt();
+		if (nth < 0) {
+			nth = -nth;
+		}
+		
+		System.out.println("You want show power of 2 table upto "+nth);
 	}
 
 }
