b258cefe1fe99af4ca114f5e685a5aff2bc2f79c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 12:01:16 +0530

    ADDED: Start Vowel Or Consonant Program With Welcome Message

---
+++ src/com/basiccoreprograms/VowelOrConsonant.java
@@ -0,0 +1,10 @@
+package com.basiccoreprograms;
+
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Vowel Or Consonant Program");
+
+	}
+
+}
