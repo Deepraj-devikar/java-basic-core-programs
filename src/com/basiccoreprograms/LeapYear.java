2256981d4dae20b2163370ff44716c0e127dbac7
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 09:45:57 +0530

    ADDED: OUTPUT year is leap year or not

--- src/com/basiccoreprograms/LeapYear.java
+++ src/com/basiccoreprograms/LeapYear.java
@@ -20,6 +20,12 @@ public class LeapYear {
 		}
 		
 		System.out.println("Year you entered : "+year);
+		
+		if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			System.out.println(year+" is leap year");
+		} else {
+			System.out.println(year+" is not leap year");
+		}
 	}
 
 }
