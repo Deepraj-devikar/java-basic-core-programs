7695454d1f80fd12221ffee21c8bed2e330793e2
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 11:08:53 +0530

    ADDED: Start Quotient And Reminder Program With Welcome Message

---
+++ src/com/basiccoreprograms/QuotientAndReminder.java
@@ -0,0 +1,10 @@
+package com.basiccoreprograms;
+
+public class QuotientAndReminder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Quotient And Reminder Program");
+
+	}
+
+}
