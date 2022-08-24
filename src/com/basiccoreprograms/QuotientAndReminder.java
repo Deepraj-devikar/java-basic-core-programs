16ba5fbf365325bbc3370629e42ad9aa753275b9
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 11:27:49 +0530

    ADDED: INPUT Dividend And Divisor

--- src/com/basiccoreprograms/QuotientAndReminder.java
+++ src/com/basiccoreprograms/QuotientAndReminder.java
@@ -1,9 +1,32 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class QuotientAndReminder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Quotient And Reminder Program");
+		
+		Scanner scanner = new Scanner(System.in);
+		// INPUT dividend
+		int dividend = 0;
+		while (dividend == 0) {
+			System.out.print("Enter dividend : ");
+			dividend = scanner.nextInt();	
+		}
+		
+		// INPUT divisor
+		int divisor = 0;
+		while (divisor == 0) {
+			System.out.print("Enter dividend : ");
+			divisor = scanner.nextInt();
+			if (divisor == 0) {
+				divisor = 1;
+			}	
+		}
+		
+		System.out.println("Dividend is "+dividend);
+		System.out.println("Divisor is "+divisor);
 
 	}
 
