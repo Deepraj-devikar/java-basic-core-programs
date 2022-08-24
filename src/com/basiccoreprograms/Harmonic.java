c6c9cdacc81003cf4b1ee0600eec5199d18f6633
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 10:17:26 +0530

    ADDED: INPUT nth number for Harmonic Number (not have to be ZERO)

--- src/com/basiccoreprograms/Harmonic.java
+++ src/com/basiccoreprograms/Harmonic.java
@@ -1,10 +1,25 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class Harmonic {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Harmonic Number Program");
 
+		// INPUT Nth number which have to calculate Harmonic Number
+		Scanner scanner = new Scanner(System.in);
+		int nth = 0;
+		while (nth == 0) {
+			System.out.print("Enter positive number : ");
+			nth = scanner.nextInt();
+			if (nth < 0) {
+				nth = -nth;
+			}	
+		}
+		
+		System.out.println("You have to show "+nth+"s Harmonic Number");
+		
 	}
 
 }
