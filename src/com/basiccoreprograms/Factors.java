0ee42f23d4cf60dfed7f748e87c093533fcf0d68
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 10:42:03 +0530

    ADDED: INPUT number to find Factors

--- src/com/basiccoreprograms/Factors.java
+++ src/com/basiccoreprograms/Factors.java
@@ -1,10 +1,24 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class Factors {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Factors Program");
 
-	}
+		// INPUT Nth number which have to calculate Factors
+		Scanner scanner = new Scanner(System.in);
+		int number = 0;
+		while (number == 0) {
+			System.out.print("Enter positive number : ");
+			number = scanner.nextInt();
+			if (number < 0) {
+				number = -number;
+			}	
+		}
+		
+		System.out.println("You have to show "+number+"s Factors");
 
+	}
 }
