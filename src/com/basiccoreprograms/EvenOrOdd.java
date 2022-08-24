db2990adb344aba0d3ac5d72b60dc5c5c491ced5
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 11:55:11 +0530

    ADDED: INPUT number to check even or odd

--- src/com/basiccoreprograms/EvenOrOdd.java
+++ src/com/basiccoreprograms/EvenOrOdd.java
@@ -1,9 +1,19 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Even Or Odd Program");
+		
+		Scanner scanner = new Scanner(System.in);
+		// INPUT number
+		int number = 0;
+		System.out.print("Enter number : ");
+		number = scanner.nextInt();
+		
+		System.out.println("You entered "+number);
 
 	}
 
