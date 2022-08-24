b8544bb1f124da53f8f3cfdd3e055236bc6ce5f7
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 12:05:40 +0530

    ADDED: INPUT character to check vowel or consonant

--- src/com/basiccoreprograms/VowelOrConsonant.java
+++ src/com/basiccoreprograms/VowelOrConsonant.java
@@ -1,9 +1,19 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class VowelOrConsonant {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Vowel Or Consonant Program");
+		
+		Scanner scanner = new Scanner(System.in);
+		// INPUT character
+		String character;
+		System.out.print("Enter character : ");
+		character = scanner.nextLine();
+		
+		System.out.println("You entered character "+character);
 
 	}
 
